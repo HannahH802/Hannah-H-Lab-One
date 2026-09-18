2b53a4a72bd348f2ec38e6fd0e708dbc4405e6b2
HannahH802 <hhornyak29@setoncatholic.org>
2026-09-18 16:02:28 +0000

    Extra credit commit

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 name = input("What is your name: ")
 print(name)
-age = input("What is your age:")
+age = int(input("What is your age: "))
 print(age)
 color = input("What is your favorite color:")
 print(color)
 print("Hello", name , "you are", age ," years old and your favorite color is ", color)
+
+if age < 13:
+	print("You are a kid - enjoy being young!")
+elif age < 20:
+	print("You are a teenager - exciting years ahead!")
+elif age < 65:
+	print("You are an adult - keep pursuing your goals!")
+else:
+	print("You are a senior - enjoy sharing your wisdom!")
